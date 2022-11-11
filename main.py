6a5fd1e11ff72477a7520ea4eee99e1e23c3b360
Eduardo Adoglio <eduardoadoglio4@gmail.com>
2022-11-11 17:23:00 -0300

    More WIP

--- main.py
+++ main.py
@@ -7,10 +7,14 @@ from sklearn.decomposition import PCA
 from sklearn.preprocessing import LabelEncoder
 from sklearn.metrics import plot_confusion_matrix
 import transformers
+from transformers import AdamW
 from transformers import AutoModel, BertTokenizerFast
 import torch
 import torch.nn as nn
 from torch.utils.data import TensorDataset, DataLoader, RandomSampler, SequentialSampler
+from sklearn.utils.class_weight import compute_class_weight
+
+from bert_arch import BERT_Arch
 
 
 data = pd.read_csv("pre_processed_data.csv")
@@ -109,3 +113,182 @@ val_sampler = SequentialSampler(val_data)
 
 # dataloader for validation set
 val_dataloader = DataLoader(val_data, sampler=val_sampler, batch_size=batch_size)
+
+for param in bert.parameters():
+    param.requires_grad = False
+
+model = BERT_Arch(bert)
+
+# define the optimizer
+optimizer = AdamW(
+    model.parameters(),
+    lr=1e-5,  # learning rate
+)
+
+# compute the class weights
+class_weights = compute_class_weight(
+    class_weight="balanced", classes=np.unique(train_labels), y=train_labels
+)
+
+print("Class Weights:", class_weights)
+
+weights = torch.tensor(class_weights, dtype=torch.float)
+
+# define the loss function
+cross_entropy = nn.NLLLoss(weight=weights)
+
+# number of training epochs
+epochs = 10
+
+
+def train():
+
+    model.train()
+
+    total_loss, total_accuracy = 0, 0
+
+    # empty list to save model predictions
+    total_preds = []
+
+    # iterate over batches
+    for step, batch in enumerate(train_dataloader):
+        print(f"{step = } / {len(train_dataloader)}")
+        # progress update after every 50 batches.
+        if step % 50 == 0 and not step == 0:
+            print("  Batch {:>5,}  of  {:>5,}.".format(step, len(train_dataloader)))
+
+        # push the batch to gpu
+        batch = [r for r in batch]
+        sent_id, mask, labels = batch
+        # print(type(labels),type(mask),type(sent_id))
+        # print(sent_id)
+        # clear previously calculated gradients
+        model.zero_grad()
+        # get model predictions for the current batch
+        preds = model(sent_id, mask)
+
+        # compute the loss between actual and predicted values
+        loss = cross_entropy(preds, labels)
+
+        # add on to the total loss
+        total_loss = total_loss + loss.item()
+
+        # backward pass to calculate the gradients
+        loss.backward()
+
+        # clip the the gradients to 1.0. It helps in preventing the exploding gradient problem
+        torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
+
+        # update parameters
+        optimizer.step()
+
+        # model predictions are stored on GPU. So, push it to CPU
+        preds = preds.detach().cpu().numpy()
+
+        # append the model predictions
+        total_preds.append(preds)
+
+    # compute the training loss of the epoch
+    avg_loss = total_loss / len(train_dataloader)
+
+    # predictions are in the form of (no. of batches, size of batch, no. of classes).
+    # reshape the predictions in form of (number of samples, no. of classes)
+    total_preds = np.concatenate(total_preds, axis=0)
+
+    # returns the loss and predictions
+    return avg_loss, total_preds
+
+
+def evaluate():
+
+    print("\nEvaluating...")
+
+    # deactivate dropout layers
+    model.eval()
+
+    total_loss, total_accuracy = 0, 0
+
+    # empty list to save the model predictions
+    total_preds = []
+
+    # iterate over batches
+    for step, batch in enumerate(val_dataloader):
+
+        # Progress update every 50 batches.
+        if step % 50 == 0 and not step == 0:
+
+            # Calculate elapsed time in minutes.
+            # elapsed = format_time(time.time() - t0)
+
+            # Report progress.
+            print("  Batch {:>5,}  of  {:>5,}.".format(step, len(val_dataloader)))
+
+        # push the batch to gpu
+        batch = [t for t in batch]
+
+        sent_id, mask, labels = batch
+
+        # deactivate autograd
+        with torch.no_grad():
+
+            # model predictions
+            preds = model(sent_id, mask)
+
+            # compute the validation loss between actual and predicted values
+            loss = cross_entropy(preds, labels)
+
+            total_loss = total_loss + loss.item()
+
+            preds = preds.detach().cpu().numpy()
+
+            total_preds.append(preds)
+
+    # compute the validation loss of the epoch
+    avg_loss = total_loss / len(val_dataloader)
+
+    # reshape the predictions in form of (number of samples, no. of classes)
+    total_preds = np.concatenate(total_preds, axis=0)
+
+    return avg_loss, total_preds
+
+
+best_valid_loss = float("inf")
+
+# empty lists to store training and validation loss of each epoch
+train_losses = []
+valid_losses = []
+
+# for each epoch
+for epoch in range(epochs):
+
+    print("\n Epoch {:} / {:}".format(epoch + 1, epochs))
+
+    # train model
+    train_loss, _ = train()
+
+    # evaluate model
+    valid_loss, _ = evaluate()
+
+    # save the best model
+    if valid_loss < best_valid_loss:
+        best_valid_loss = valid_loss
+        torch.save(model.state_dict(), "saved_weights.pt")
+
+    # append training and validation loss
+    train_losses.append(train_loss)
+    valid_losses.append(valid_loss)
+
+    print(f"\nTraining Loss: {train_loss:.3f}")
+    print(f"Validation Loss: {valid_loss:.3f}")
+
+
+# load weights of best model
+path = "saved_weights.pt"
+model.load_state_dict(torch.load(path))
+
+with torch.no_grad():
+    preds = model(test_seq, test_mask)
+    preds = preds.detach().cpu().numpy()
+
+preds = np.argmax(preds, axis=1)
+print(classification_report(test_y, preds))
